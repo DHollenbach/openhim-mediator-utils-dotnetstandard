a7ecb27ac88070ca7597b97fcc4ce93cdd762fe1
Dane Hollenbach <danehollenbach@gmail.com>
2018-08-07 12:31:49 +0200

    Add to test classes to test helpers.

---
+++ HealthMediator.Utils.Tests/HealthUtilTests.cs
@@ -0,0 +1,8 @@
+using Xunit;
+
+namespace HealthMediator.Utils.Tests
+{
+	public class HealthUtilTests
+    {
+    }
+}
---
+++ HealthMediator.Utils.Tests/RestSharpExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using HealthMediator.Utils.Helpers;
+using Moq;
+using RestSharp;
+using System;
+using Xunit;
+
+namespace HealthMediator.Utils.Tests
+{
+	public class RestSharpExtensionsTests
+    {
+		[Fact]
+		public void GetRestResponseException_GivenNullIRestResponse_ShouldThrowArgumentNullException()
+		{
+			IRestResponse restRespone = null;
+			Assert.Throws<ArgumentNullException>(() => restRespone.GetFailedRestResponseException());
+		}
+
+		[Fact]
+		public void GetRestResponseException_GivenIRestResponseIsSuccessful_ShouldReturnNullException()
+		{
+			// Arrange
+			var mock = new Mock<IRestResponse>();
+			mock.Setup(x => x.IsSuccessful).Returns(true);
+			IRestResponse restRespone = mock.Object;
+
+			// Act
+			var result = restRespone.GetFailedRestResponseException();
+
+			// Assert
+			Assert.Null(result);
+		}
+
+		[Fact]
+		public void GetRestResponseException_GivenIRestResponseIsNotSuccessful_ShouldReturnException()
+		{
+			// Arrange
+			var defaultMessage = "not specified";
+			var mock = new Mock<IRestResponse>();
+			mock.Setup(x => x.IsSuccessful).Returns(false);
+
+			IRestResponse restRespone = mock.Object;
+
+			// Act
+			var result = restRespone.GetFailedRestResponseException();
+
+			// Assert
+			Assert.NotNull(result);
+		}
+	}
+}
